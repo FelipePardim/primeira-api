83ca8c7dce93b8230a676fa5c01c7cd6a9e2b820
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2025-05-22 20:06:00 -0300

    Esse ẽ o primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const PORTA = 3000;
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (request, response) => {
+ response.json({"dados":"Mil pessoas foram ontem"});
+})
+
+server.get("/senai", (request, response) => {
+ response.json({"cursos":"5 cursos"});
+})
+
+server.post("/", (request, response) => {
+ console.log(request.body);
+
+ response.json(request.body);
+})
+
+let carros = [];
+
+server.get("/carros", (request, response) => {
+ response.json(carros);
+})
+
+server.post("/carros", (request, response) => {
+ console.log(request.body);
+
+ carros.push(request.body)
+
+ response.sendStatus(201);
+})
+
+server.listen(PORTA, () => console.log("Meu servidor tá funcionando na porta:", PORTA));
