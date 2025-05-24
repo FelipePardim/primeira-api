527f3160c92a5a1b913d4de3506f2c6888821353
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2025-05-23 22:17:34 -0300

    Adicionando patch e delete

--- index.js
+++ index.js
@@ -6,32 +6,81 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (request, response) => {
- response.json({"dados":"Mil pessoas foram ontem"});
-})
+let carros = [
+ {
+  "id": 1,
+  "nome" : "Fusca",
+  "marca" : "VolksWagen",
+  "ano" : "1970",
+  "tipo": "hatch"
+ },
+ {
+  "id": 2,
+  "nome" : "Uno",
+  "marca" : "Fiat",
+  "ano" : "2005",
+  "tipo": "hatch"
+ },{
+  "id": 3,
+  "nome" : "Uno",
+  "marca" : "Fiat",
+  "ano" : "2010",
+  "tipo": "hatch"
+ },
+];
 
-server.get("/senai", (request, response) => {
- response.json({"cursos":"5 cursos atuais"});
-})
-
-server.post("/", (request, response) => {
- console.log(request.body);
-
- response.json(request.body);
-})
-
-let carros = [];
+let ultimoId = carros.length;
 
 server.get("/carros", (request, response) => {
  response.json(carros);
 })
 
 server.post("/carros", (request, response) => {
- console.log(request.body);
+ console.log("Criando o novo carro: ", request.body);
+
+ ultimoId++;
+ request.body.id = ultimoId;
 
  carros.push(request.body)
 
  response.sendStatus(201);
+})
+
+server.get("/carros/:id", (request, response) => {
+ const indexCarro = carros.findIndex(carro => carro.id === Number(request.params.id));
+
+ if (indexCarro == -1) {
+  response.sendStatus(404);
+ } else {
+  response.json(carros[indexCarro]);
+ }
+})
+
+
+server.patch("/carros/:id", (request, response) => {
+ const indexCarro = carros.findIndex(carro => carro.id === Number(request.params.id));
+
+ if (indexCarro == -1) {
+  response.sendStatus(404);
+ } else {
+  
+  request.body.id = carros[indexCarro].id;
+
+  carros[indexCarro] = request.body;
+  response.json(carros[indexCarro]);
+ }
+})
+
+server.delete("/carros/:id", (request, response) => {
+ const indexCarro = carros.findIndex(carro => carro.id === Number(request.params.id));
+
+ if (indexCarro == -1) {
+  response.sendStatus(404);
+ } else {
+  carros.splice(indexCarro, 1);
+
+  response.sendStatus(200);
+ }
 })
 
 server.listen(PORTA, () => console.log("Meu servidor tá funcionando na porta:", PORTA));
